03325fc7bb0acfcd420e80c5da3f43ed6bc0c769
Leonardo Lemes <leonardo_lemes@mckinsey.com>
2023-09-14 17:08:01 -0300

    feat: tsid generation

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import { randomBytes } from "crypto";
+
+class Tsid {
+	static generate(
+		idSizeInBits: number = 64,
+		timeComponentSizeInBits: number = 42,
+		randomComponentSizeInBits: number = 22,
+		epoch: Date = new Date("2020-01-01"),
+	): bigint {
+		if (idSizeInBits % 8 !== 0) throw new Error("idSizeInBits must be a multiple of 8");
+		if (idSizeInBits !== timeComponentSizeInBits + randomComponentSizeInBits)
+			throw new Error("idSizeInBits must be the sum of timeComponentSizeInBits and randomComponentSizeInBits");
+
+		const bytes = randomBytes(idSizeInBits / 8);
+		const timeComponent = Tsid.getTimeComponent(epoch);
+		const randomComponent = Tsid.getRandomComponent(bytes);
+		const id = this.composeId(randomComponentSizeInBits, timeComponent, randomComponent);
+
+		return id;
+	}
+
+	private static getTimeComponent(epoch: Date): number {
+		return Date.now() - epoch.getTime();
+	}
+
+	private static getRandomComponent(bytes: Buffer): bigint {
+		let value = BigInt(0);
+		for (let i = 0; i < bytes.length; i++) {
+			value |= BigInt(bytes[i]) << BigInt(bytes.length * bytes.byteLength - bytes.byteLength * (i + 1));
+		}
+		return value;
+	}
+
+	private static composeId(
+		randomComponentSizeInBits: number,
+		timeComponent: number,
+		randomComponent: bigint,
+	): bigint {
+		const randomComponentMask = ~BigInt(~BigInt(0) << BigInt(randomComponentSizeInBits));
+		const id =
+			(BigInt(timeComponent) << BigInt(randomComponentSizeInBits)) | (randomComponent & randomComponentMask);
+		return id;
+	}
+}
+
+console.log(Tsid.generate());
